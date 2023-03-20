0f06a44ad6b7cd20e1652041e55d8d33a8311002
Hà Gia Kính <giakinh2000@gmail.com>
2023-03-20 09:45:41 +0700

    update

--- src/main/java/fpt/edu/shopping/constant/OrderStatus.java
+++ src/main/java/fpt/edu/shopping/constant/OrderStatus.java
@@ -3,5 +3,5 @@ package fpt.edu.shopping.constant;
 public class OrderStatus {
     public static final Integer SUCCESS = 1;
     private static final Integer PENDING = 0;
-    private static final Integer FAILED = 0;
+    private static final Integer FAILED = 2;
 }
