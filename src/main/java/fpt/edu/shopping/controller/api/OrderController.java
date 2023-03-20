171811cfe97076cd1107b7f72250db11c0d9d9e3
Do Nhat Duc <duc21102001@gmail.com>
2023-03-20 22:53:17 +0700

    update

--- src/main/java/fpt/edu/shopping/controller/api/OrderController.java
+++ src/main/java/fpt/edu/shopping/controller/api/OrderController.java
@@ -1,9 +1,45 @@
 package fpt.edu.shopping.controller.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fpt.edu.shopping.entity.Order;
+import fpt.edu.shopping.entity.Product;
+import fpt.edu.shopping.service.OrderService;
+import fpt.edu.shopping.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return new ResponseEntity<>(orderService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createOrder(@RequestBody Order order) {
+        orderService.create(order);
+        return new ResponseEntity<>("Create order success", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateOrder(@RequestBody Order order) {
+        orderService.update(order);
+        return new ResponseEntity<>("update order success", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteOrder(@PathVariable Long id) {
+        orderService.delete(id);
+        return new ResponseEntity<>("delete order success", HttpStatus.OK);
+    }
 }
--- src/main/java/fpt/edu/shopping/service/impl/OrderServiceImpl.java
+++ src/main/java/fpt/edu/shopping/service/impl/OrderServiceImpl.java
@@ -1,33 +1,51 @@
 package fpt.edu.shopping.service.impl;
 
 import fpt.edu.shopping.entity.Order;
+import fpt.edu.shopping.entity.Product;
 import fpt.edu.shopping.repository.OrderRepository;
+import fpt.edu.shopping.repository.ProductRepository;
 import fpt.edu.shopping.service.OrderService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
-@AllArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
     @Override
     public void create(Order orders) {
-
+        orderRepository.save(orders);
     }
     @Override
     public List<Order> getAll() {
-        return null;
+        List<Order> listOrders = new ArrayList<>();
+        List<Order> orders = orderRepository.findAll();
+
+        return listOrders;
     }
 
     @Override
     public void update(Order order) {
-
+        Order orderFind = orderRepository.findById(order.getId()).orElseThrow(() -> new RuntimeException("Not found order"));
+        orderFind.setOrderItems(order
+                .getOrderItems());
+        orderFind.setStatus(order.getStatus());
+        orderFind.setTotalPrice(order.getTotalPrice());
+        orderFind.setCreatedAt(order.getCreatedAt());
+        orderFind.setUpdatedAt(order.getUpdatedAt());
+        orderRepository.save(orderFind);
     }
 
     @Override
     public void delete(Long orderId) {
-
+        orderRepository.deleteById(orderId);
     }
 }
